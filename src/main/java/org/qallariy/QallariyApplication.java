ecfdda0a37dd53d46d1079ad1d60e6399eb9ed1c
adriansotoh <adriansotohidalgo16@gmail.com>
2022-05-25 23:56:26 -0500

    Creacion del proyecto qullariy

---
+++ src/main/java/org/qallariy/QallariyApplication.java
@@ -0,0 +1,13 @@
+package org.qallariy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QallariyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QallariyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/qallariy/QallariyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qallariy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QallariyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
